29fa7e6970e129b4211cfa05138a36da5fb21b24
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2022-09-25 21:20:24 +0200

    About me added

--- cogs/normal_cmds.py
+++ cogs/normal_cmds.py
@@ -14,6 +14,12 @@ class commands(commands.Cog, description="Normal user commands for everyone!"):
     async def ping(self, ctx):
         embed = discord.Embed(description=f'**🏓 Pong** `{round(self.client.latency * 1000)}ms`', color=discord.Colour.random())
         await ctx.send(embed=embed)
+     
+    @commands.command(aliases=["abt"], help="Shows everything about me. Usage: `%about`")
+    async def about(self, ctx):
+        embed = discord.Embed(title=f"Informations about the Bot", description=f"Hello, i am {self.client.user.name}. I am a small side Project from me **Joniii#0159** the Developer. I am in the open alpha phase at the moment, and kinda bad. I am 1 Month old now and have already many commands.", color=discord.Colour.random())
+        embed.add_field(name="Informations About the Developer", value="Hi! I am Jonas. I am 14 Years old and from germany. I like coding bots in python, i learned that all by myself and some YouTube Tutorials. I am currently in the 10th grade and will finish the school in about 1 year.", inline=False)
+        await ctx.send(embed=embed)
     
 def setup(client):
     client.add_cog(commands(client))
