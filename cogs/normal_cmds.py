d2b5218ae0a336e7b3f0610513cdb1743c22b53b
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2022-09-25 21:11:11 +0200

    added ping command

--- cogs/normal_cmds.py
+++ cogs/normal_cmds.py
@@ -9,6 +9,11 @@ class commands(commands.Cog, description="Normal user commands for everyone!"):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{self.__class__.__name__} cog wurde geladen\n-----")
+     
+    @commands.command(aliases=["p"], help="This command shows the ping of the bot. Usage: `%ping`")
+    async def ping(self, ctx):
+        embed = discord.Embed(description=f'**🏓 Pong** `{round(self.client.latency * 1000)}ms`', color=discord.Colour.random())
+        await ctx.send(embed=embed)
     
 def setup(client):
     client.add_cog(commands(client))
