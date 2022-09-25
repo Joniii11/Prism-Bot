612c0ff6760fa06b96e2b9e521df997ba3e7cbdf
Joniii11 <88550325+Joniii11@users.noreply.github.com>
2022-09-25 20:54:21 +0200

    Updated main, added reconnect

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import os
+
+from pathlib import Path
+from discord.ext import commands
+
+PREFIX = "%"
+TOKEN = ""
+
+cwd = Path(__file__).parents[0]
+cwd = str(cwd)
+print(f"{cwd}\n-----")
+
+client = commands.Bot(command_prefix=PREFIX, help_command=None, intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("-----\nEingelogt als: {} : {}\n-----\nMeine standart Prefix ist zurzeit: {}\n-----".format(client.user.name, client.user.id, PREFIX))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(f"Prefix: {PREFIX} | For help: {PREFIX}help"))
+ 
+if __name__ == '__main__':
+    for file in os.listdir(cwd+"/cogs"):
+        if file.endswith(".py") and not file.startswith("_"):
+            client.load_extension(f"cogs.{file[:-3]}")
+
+
+client.run(TOKEN, reconnect=True)
